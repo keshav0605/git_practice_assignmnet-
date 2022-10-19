b6e27304c2c76e1a4f73cae777d0a0348715a46d
keshav0605 <keshav06patel@gmail.com>
2022-10-19 23:14:54 +0530

    prime and palindrome problems

---
+++ primeproblem.js
@@ -0,0 +1,12 @@
+let num = 13;
+let count =0;
+for (let x = 0 ; x <= 13 ; x++){
+    if(num%x==0){
+        count++
+    }
+}
+if(count==2){
+    console.log(num,"is not prime")
+}else{
+    console.log(num,"is prime")
+}
